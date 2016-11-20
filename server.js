6bad6cda7b6dbf84d09ca1236c5296497e704794
Deepak Aitha <Deepak Aitha>
2016-11-20 02:24:28 -0500

    uploading server file

---
+++ server.js
@@ -0,0 +1,59 @@
+﻿// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var amazon = require('amazon-product-api');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;  // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+ var keyword=req.query.keyword;
+    var client = amazon.createClient({
+      awsId: "AKIAJRWT3AWHMGGLQUJA",
+      awsSecret: "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",
+      awsTag: "hackohio-20"
+    });
+    client.itemSearch({
+     SearchIndex: 'All',
+     Keywords: keyword,
+     ResponseGroup: 'ItemAttributes,Offers,Images'
+    }).then(function(results){
+     var items = [];
+     var item;
+     for(var i = 0; i <10 ; i++){
+       item= {
+          detailpageurl: results[i].DetailPageURL,
+          medimg: results[i].MediumImage[0].URL,
+          title: results[i].ItemAttributes[0].Title
+       };
+       items.push(item);
+    }
+      res.json(items);
+      //var price = results[0].Offers[0].Offer[0].OfferListing[0].Price[0].FormattedPrice;
+});
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
